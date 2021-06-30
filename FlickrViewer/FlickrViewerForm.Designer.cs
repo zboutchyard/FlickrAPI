ac9323e573c2c616fa01ed3f5937bed338d64c06
zboutchyard <86641746+zboutchyard@users.noreply.github.com>
2021-06-30 18:21:41 -0400

    Add files via upload

---
+++ FlickrViewer/FlickrViewerForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace FlickrViewer
+{
+    partial class FlickrViewerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imagesListBox = new System.Windows.Forms.ListBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imagesListBox
+            // 
+            this.imagesListBox.FormattingEnabled = true;
+            this.imagesListBox.Location = new System.Drawing.Point(11, 41);
+            this.imagesListBox.Name = "imagesListBox";
+            this.imagesListBox.Size = new System.Drawing.Size(149, 394);
+            this.imagesListBox.TabIndex = 0;
+            this.imagesListBox.SelectedIndexChanged += new System.EventHandler(this.imagesListBox_SelectedIndexChanged);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(166, 41);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(567, 394);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Flickr search tags here: ";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(166, 5);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(456, 20);
+            this.inputTextBox.TabIndex = 3;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(628, 3);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(105, 23);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // FlickrViewerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 450);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.imagesListBox);
+            this.Name = "FlickrViewerForm";
+            this.Text = "Flickr Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox imagesListBox;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ FlickrViewer/FlickrViewerForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace FlickrViewer
+{
+    public partial class FlickrViewerForm : Form
+    {
+        private const string KEY = "fea1ba257a70522c6a3c27585f70664a";
+
+        private static HttpClient flickrClient = new HttpClient();
+        Task<string> flickrTask = null;
+        public FlickrViewerForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void searchButton_Click(object sender, EventArgs e)
+        {
+            if (flickrTask?.Status != TaskStatus.RanToCompletion)
+            {
+                var result = MessageBox.Show(
+                    "Cancel the current Flickr search?",
+                    "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.No)
+                {
+                    return;
+                }
+                else
+                {
+                    flickrClient.CancelPendingRequests();
+                }
+            }
+            var flickrURL = "https://api.flickr.com/services/rest/?method=" +
+                $"flickr.photos.search&api_key={KEY}&" +
+                $"tags={inputTextBox.Text.Replace(" ", ",")}" +
+                "&tag_mode=all&per_page=500&privacy_filter=1";
+
+            imagesListBox.DataSource = null;
+            imagesListBox.Items.Clear();
+            pictureBox.Image = null;
+            imagesListBox.Items.Add("Loading...");
+
+            flickrTask = flickrClient.GetStringAsync(flickrURL);
+            XDocument flickrXML = XDocument.Parse(await flickrTask);
+
+            var flickrPhotos =
+                from photo in flickrXML.Descendants("photo")
+                let id = photo.Attribute("id").Value
+                let title = photo.Attribute("title").Value
+                let secret = photo.Attribute("secret").Value
+                let server = photo.Attribute("server").Value
+                let farm = photo.Attribute("farm").Value
+                select new FlickrResult
+
+                {
+                    Title = title,
+                    URL = $"https://farm{farm}.staticflickr.com/" +
+                    $"{server}/{id}_{secret}.jpg"
+                };
+            imagesListBox.Items.Clear();
+
+            if (flickrPhotos.Any())
+            {
+                imagesListBox.DataSource = flickrPhotos.ToList();
+                imagesListBox.DisplayMember = "Title";
+            }
+            else
+            {
+                imagesListBox.Items.Add("No Matches");
+            }
+
+        }
+
+        private async void imagesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (imagesListBox.SelectedItem != null)
+            {
+                string selectedURL =
+                    ((FlickrResult)imagesListBox.SelectedItem).URL;
+                byte[] imageBytes =
+                    await flickrClient.GetByteArrayAsync(selectedURL);
+
+                MemoryStream memoryStream = new MemoryStream(imageBytes);
+                pictureBox.Image = Image.FromStream(memoryStream);
+            }
+        }
+    }
+}
